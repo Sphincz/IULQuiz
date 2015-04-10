a15dc443361476a8029199f7a6df8de0e2d9b6f3
netgrade <ivomdasilva@gmail.com>
2015-04-10 23:40:46 +0100

    no more sql injections! :D

--- src/pt/iul/sida/iulquiz/QuizBD.java
+++ src/pt/iul/sida/iulquiz/QuizBD.java
@@ -2,6 +2,7 @@ package pt.iul.sida.iulquiz;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -41,20 +42,25 @@ public class QuizBD {
 		boolean Email_Nao_Existente;
 			
 		try {
-		statement = con.createStatement();
-		result = statement.executeQuery("SELECT * FROM Estudante WHERE Estudante.Email_Aluno = '"+user.getEmail()+"';");
+			
+			String selectStatement = "SELECT * FROM Estudante WHERE Estudante.Email_Aluno = ?";
+			PreparedStatement prepStmt = con.prepareStatement(selectStatement);
+			prepStmt.setString(1,user.getEmail());
+			ResultSet rs = prepStmt.executeQuery();
+//		statement = con.createStatement();
+	//	result = statement.executeQuery("SELECT * FROM Estudante WHERE Estudante.Email_Aluno = '"+user.getEmail()+"';");
 		
-		if(result.next()) {
+		if(rs.next()) {
 			System.out.println("Email já registado na QuizBD.");
 			Email_Ja_Registado = true;
-			statement.close();
-			result.close();
+			prepStmt.close();
+			rs.close();
 			return Email_Ja_Registado;
 		} else {
 			System.out.println("Email não registado na QuizBD.");
 			Email_Nao_Existente = false;
-			statement.close();
-			result.close();
+			prepStmt.close();
+			rs.close();
 			return Email_Nao_Existente;
 		}
 		
@@ -69,19 +75,42 @@ public class QuizBD {
 		boolean Registo_Efectuado = false;
 		
 		try{
-			statement = con.createStatement();
+			
+			PreparedStatement prepStmt;
+//			ResultSet rs;
+			//statement = con.createStatement();
 			
 			if(user.getCurso().equals("Curso: (opcional)")) {
-				statement.executeQuery("INSERT INTO Estudante VALUES ('"+user.getEmail()+"', NULL, NULL, '"+user.getNome()+"', '"+generatedPassword+"', '"+user.getPergunta()+"', '"+user.getResposta()+"');");
+				String insertStatement = "INSERT INTO Estudante VALUES (?, NULL, NULL, ?, ?, ?,?)";
+				prepStmt = con.prepareStatement(insertStatement);
+				prepStmt.setString(1,user.getEmail());
+				prepStmt.setString(2,user.getNome());
+				prepStmt.setString(3,generatedPassword);
+				prepStmt.setString(4,user.getPergunta());
+				prepStmt.setString(5,user.getResposta());
+				prepStmt.executeQuery();
+				
+				//statement.executeQuery("INSERT INTO Estudante VALUES ('"+user.getEmail()+"', NULL, NULL, '"+user.getNome()+"', '"+generatedPassword+"', '"+user.getPergunta()+"', '"+user.getResposta()+"');");
 			} else {
 				System.out.println(user.getSigla_Curso());
-				statement.executeQuery("INSERT INTO Estudante VALUES ('"+user.getEmail()+"', '"+user.getSigla_Curso()+"', '"+user.getCurso()+"', '"+user.getNome()+"', '"+generatedPassword+"', '"+user.getPergunta()+"', '"+user.getResposta()+"');");	
+				//statement.executeQuery("INSERT INTO Estudante VALUES ('"+user.getEmail()+"', '"+user.getSigla_Curso()+"', '"+user.getCurso()+"', '"+user.getNome()+"', '"+generatedPassword+"', '"+user.getPergunta()+"', '"+user.getResposta()+"');");	
+				String insertStatement = "INSERT INTO Estudante VALUES (?,?,?, ?, ?, ?,?)";
+				prepStmt = con.prepareStatement(insertStatement);
+				prepStmt.setString(1,user.getEmail());
+				prepStmt.setString(2,user.getSigla_Curso());
+				prepStmt.setString(3,user.getCurso());
+				prepStmt.setString(4,user.getNome());
+				prepStmt.setString(5,generatedPassword);
+				prepStmt.setString(6,user.getPergunta());
+				prepStmt.setString(7,user.getResposta());
+				prepStmt.executeQuery();
+			
 			}
 			
 			System.out.println("Insert com sucesso na QuizDB.");
 			Registo_Efectuado = true;
-			statement.close();
-			result.close();
+			prepStmt.close();
+//			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null, "Ocorreu um erro na inserção do utilizador na base de dados QuizBD.");
