00715175238a95fb9917cffd269fd7f91f21014c
Sphincz <raimundo.asl@gmail.com>
2015-04-10 22:15:49 +0100

    + Minor fix.

--- src/pt/iul/sida/iulquiz/ISCTEBD.java
+++ src/pt/iul/sida/iulquiz/ISCTEBD.java
@@ -16,12 +16,6 @@ public class ISCTEBD {
 	/** Uma query de acesso a base de dados. */
 	private Statement statement;
 	
-	/** O URL para estabelecer ligacao a base de dados. localhost2638 significa que o servidor da base de dados e local, ligado na porta 2638. O nome da base de dados e ISCTEDB. */
-	private String dburl = "jdbc:sqlanywhere:Tds:localhost:2638?eng=ISCTEBD";
-	
-	/** O username e password de acesso a base de dados */
-	private String user = "Fenix", password = "FenixDB";
-	
 	/** A ligacao a base de dados. */
 	private Connection con;
 	
@@ -31,7 +25,7 @@ public class ISCTEBD {
 	public ISCTEBD() {
         // Connect to Sybase Database
         try {
-			con = DriverManager.getConnection(dburl, user, password);
+			con = DriverManager.getConnection("jdbc:sqlanywhere:uid=Fenix;pwd=FenixDB;eng=ISCTEDB");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null, "ISCTEDB - Base de dados desligada ou username e/ou password inválidos!");
